3a1d1bbd942edec0c11f7650c5d0cabe47595cef
Galen Frechette <galenfrechette@gmail.com>
2015-10-13 15:10:33 -0700

    Set deploy config to deploy to master

--- config.rb
+++ config.rb
@@ -1,9 +1,7 @@
 activate :deploy do |deploy|
   deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = "custom-remote" # remote name or git url, default: origin
-  # deploy.branch   = "custom-branch" # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  deploy.branch = 'master'
+  deploy.build_before = true
 end
 
 ###
